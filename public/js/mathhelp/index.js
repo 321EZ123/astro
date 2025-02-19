5c2dac67eafb7d78abccfd86c6ce131f88eedbc3
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2025-02-19 14:27:52 -0800

    Update index.js

--- public/js/mathhelp/index.js
+++ public/js/mathhelp/index.js
@@ -1,14 +1,15 @@
 "use strict";
-
 /**
  * @type {HTMLFormElement}
  */
 const form = document.getElementById("uv-form");
-
 /**
  * @type {HTMLInputElement}
  */
 const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -16,12 +17,12 @@ form.addEventListener("submit", async (event) => {
   try {
     await registerSW();
   } catch (err) {
-    console.error("Service Worker Registration Failed:", err);
+    console.log(err);
     throw err;
   }
 
   const url = search(address.value, "https://www.google.com/search?q=%s");
 
-  sessionStorage.setItem("url", __uv$config.prefix + __uv$config.encodeUrl(url));
-  location.href = "/go/";
+  sessionStorage.setItem("url", __uv$config.prefix + __uv$config.encodeUrl(url))
+  location.href = "/go/"
 });
